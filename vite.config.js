af38745eb75b4e42e9f10c21ee13567c5ffb8e3f
Adam Schaub <adam.m.schaub@gmail.com>
2025-09-10 04:32:14 +0000

    Update vite base to match repo name

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/palia-timer/',
   test: {
     globals: true,
     environment: 'jsdom',
